cf78f3a6e3a48385579a34b2e4d4381f5fec68cb
Rick Slinkman <rw.slinkman@gmail.com>
2020-05-10 17:17:33 +0200

    Source code clean up and README update.

--- run-ci.js
+++ run-ci.js
@@ -1,22 +1,28 @@
 let ContractPolice = require("./index.js");
 
-const env = process.env;
-console.log(env);
-// TODO: Get config from ENV
-const contractsDirectory = "/contractpolice/ci-contracts";
+// Define used variables used by ContractPolice, injected by Docker
 const targetUrl = process.env.CP_TARGET;
-let config = {
-    reportOutputDir: "outputs"
-};
-let contractPolice = new ContractPolice(contractsDirectory, targetUrl, config);
+const contractsDirectory = "/contractpolice/ci-contracts";
+const configFailOnError = (process.env.CP_FAIL_ON_ERROR !== 'false');
+const configReporter = (process.env.CP_REPORTER === "junit") ? "junit" : "default";
 
+// Gather injected config into single object
+let config = {
+    reportOutputDir: "outputs", // TODO: This should be absolute like the other parameters
+    failOnError: configFailOnError,
+    reporter: configReporter
+};
+
+let contractPolice = new ContractPolice(contractsDirectory, targetUrl, config);
 console.log("Start contract test(s) with ContractPolice");
 contractPolice.testContracts()
     .then(function() {
+        // Successful test, no errors found
         console.log("ContractPolice successfully finished executing contract tests");
         process.exitCode = 0; // success
     })
     .catch(function(err) {
+        // Show output of contract test
         console.error(err);
         process.exitCode = 1; // failure
     });
